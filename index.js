d2361b110f4988f2cfaef11dbe7ba655fc174cf2
aasuyadav1 <aasuy284@gmail.com>
2025-01-29 15:04:33 +0530

    fix: cors error

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import connectDb from './config/db.js';
-import userRoute from './routes/user-route.js';
-import eventRoute from './routes/event-route.js';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDb from "./config/db.js";
+import userRoute from "./routes/user-route.js";
+import eventRoute from "./routes/event-route.js";
 
 dotenv.config();
 
-
-
 const app = express();
 
-// const corsOptions = {
-//     origin: 'https://meeting-front-2vf9.vercel.app/', // Allow this origin
-//     credentials: true, // Allow credentials
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD'], // Allow required methods
-//     allowedHeaders: ['Content-Type', 'Authorization'], // Allow required headers
-//   };
-  
-//   app.use(cors(corsOptions));
+const corsOptions = {
+  origin: "https://meeting-front-2vf9.vercel.app/", // Allow this origin
+  credentials: true, // Allow credentials
+  methods: ["GET", "POST", "PUT", "DELETE", "HEAD"], // Allow required methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow required headers
+};
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
-// Middleware for parsing JSON and URL-encoded bodies
+// Middleware for pa`rsing JSON and URL-encoded bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/api', userRoute);
-app.use('/api', eventRoute);
+app.use("/api", userRoute);
+app.use("/api", eventRoute);
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Server is running' });
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Server is running" });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3899;
 
-connectDb()
-.then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`)
-    })
-})
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
